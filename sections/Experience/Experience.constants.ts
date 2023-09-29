42c459ef7bc65dcbda252c71c2bfef6081cfa854
metalboyrick <rsulisthio@gmail.com>
2023-09-29 21:34:22 +0700

    feat: update experience

--- sections/Experience/Experience.constants.ts
+++ sections/Experience/Experience.constants.ts
@@ -1,33 +1,45 @@
 export const EXPERIENCES = [
-    {
-        role: 'web platform engineer',
-        company: 'Tokopedia',
-        startDate: 'sep 2022',
-        endDate: 'nov 2022',
-        description: [
-            'implement high quality features and fixes as required for tokopedia ads.',
-            'perform TypeScript refactoring, in order to reduce editing errors.',
-            'addition of unit testing to code, increasing the unit test coverage of each component to be above 80%.',
-            'peer code review and smoke testing to ensure everything is working fine.'
-        ]
-    },
-    {
-        role: 'frontend developer',
-        company: 'Acy finance',
-        startDate: 'jul 2021',
-        endDate: 'dec 2021',
-        description: [
-            'built the MARKET page of the company with ReactJS and GraphQL to show different commodity prices and trends over time.',
-            'performed many style changes to the company product in order to enhance customer experience.oring, in order to reduce editing errors.',
-        ]
-    },
-    {
-        role: 'research assistant',
-        company: 'Tsinghua University',
-        startDate: 'mar 2021',
-        endDate: 'aug 2021',
-        description: [
-            'reimplemented and optimized the Myocardial Contrast Echocardiography Quantification Algorithm using Python OpenCV and Computer Vision algorithms to visualize heart blood flow in an intuitive manner',
-        ]
-    },
-]
+  {
+    role: "frontned engineer",
+    company: "GASPACK",
+    startDate: "apr 2023",
+    endDate: "sep 2023",
+    description: [
+      "Implement new features and fix bugs based on requirements into the GASPACK NFT Launchpad App using Next.js, TailwindCSS.",
+      "Develop NFT Minting Campaign microsites with NextJS.",
+      "Implemented Tezos Chain External Wallet Authentication with Taquito and ReactJS which allows 8000+ users to mint Tezos tokens.",
+      "Collaborate with external and internal parties to implement novel features",
+    ],
+  },
+  {
+    role: "web platform engineer",
+    company: "Tokopedia",
+    startDate: "sep 2022",
+    endDate: "nov 2022",
+    description: [
+      "implement high quality features and fixes as required for tokopedia ads.",
+      "perform TypeScript refactoring, in order to reduce editing errors.",
+      "addition of unit testing to code, increasing the unit test coverage of each component to be above 80%.",
+      "peer code review and smoke testing to ensure everything is working fine.",
+    ],
+  },
+  {
+    role: "frontend developer",
+    company: "Acy finance",
+    startDate: "jul 2021",
+    endDate: "dec 2021",
+    description: [
+      "built the MARKET page of the company with ReactJS and GraphQL to show different commodity prices and trends over time.",
+      "performed many style changes to the company product in order to enhance customer experience.oring, in order to reduce editing errors.",
+    ],
+  },
+  {
+    role: "research assistant",
+    company: "Tsinghua University",
+    startDate: "mar 2021",
+    endDate: "aug 2021",
+    description: [
+      "reimplemented and optimized the Myocardial Contrast Echocardiography Quantification Algorithm using Python OpenCV and Computer Vision algorithms to visualize heart blood flow in an intuitive manner",
+    ],
+  },
+];
