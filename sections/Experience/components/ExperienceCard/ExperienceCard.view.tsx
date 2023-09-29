5077e92826853a75e35816827056c39e0f646b2a
metalboyrick <rsulisthio@gmail.com>
2023-09-29 21:40:31 +0700

    feat: standardise experience card

--- sections/Experience/components/ExperienceCard/ExperienceCard.view.tsx
+++ sections/Experience/components/ExperienceCard/ExperienceCard.view.tsx
@@ -6,47 +6,48 @@ import Flex from "@/components/Flex";
 
 import type { ExperienceCardProps } from "./ExperienceCard.types";
 import { convertToCamelCase } from "./ExperienceCard.helpers";
-import * as css from './ExperienceCard.styles';
+import * as css from "./ExperienceCard.styles";
 
+const ExperienceCardView = (props: ExperienceCardProps) => {
+  const { isDarkTheme } = useThemeContext();
+  const {
+    role,
+    company,
+    startDate,
+    endDate = "present",
+    description,
+    isRightSided = false,
+  } = props;
 
-const ExperienceCardView = ( props: ExperienceCardProps ) => {
+  return (
+    <Flex direction="column" className={css.wrapper(isRightSided, isDarkTheme)}>
+      <div className={css.role}>
+        {role}{" "}
+        <span className={css.company(isDarkTheme)}>{`@${convertToCamelCase(
+          company
+        )}`}</span>
+      </div>
+      <div className={css.date(isDarkTheme)}>{`${startDate} - ${endDate}`}</div>
+      <Flex direction="column">
+        {description.map((item, index) => (
+          <Flex
+            key={index}
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            className={css.descGroup}
+          >
+            <CaretRightFilled className={css.descBullet(isDarkTheme)} />
+            <div className={css.descText}>{item.toLowerCase()}</div>
+          </Flex>
+        ))}
+      </Flex>
 
-    const { isDarkTheme } = useThemeContext();
-    const { role, company, startDate, endDate = 'present', description, isRightSided = false} = props;
-
-    return (
-        <Flex
-            direction="column"
-            className={css.wrapper(isRightSided, isDarkTheme)}
-        >
-            <div className={css.role}>{role}{' '} <span className={css.company(isDarkTheme)}>{`@${convertToCamelCase(company)}`}</span></div>
-            <div className={css.date(isDarkTheme)}>{`${startDate} - ${endDate}`}</div>
-            <Flex
-                direction="column"
-            >
-                {
-                    description.map((item, index) => 
-                        <Flex 
-                            key={index}
-                            justifyContent="flex-start"
-                            alignItems="flex-start"
-                            className={css.descGroup}
-                        >
-                            <CaretRightFilled className={css.descBullet(isDarkTheme)}/>
-                            <div className={css.descText}>{item}</div>
-                        </Flex>)
-                }
-                            
-            </Flex>
-            
-            <div className={css.ornament(isRightSided)}>
-                <div className={css.outerCircle(isDarkTheme)}/>
-                <div className={css.innerCircle}/>
-            </div>
-
-        </Flex>
-    );
+      <div className={css.ornament(isRightSided)}>
+        <div className={css.outerCircle(isDarkTheme)} />
+        <div className={css.innerCircle} />
+      </div>
+    </Flex>
+  );
 };
 
 export default ExperienceCardView;
-
